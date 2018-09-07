a42d973ccc6338cd22f620caf803a25fdfe791b8
ChandraSekhar <41725757+CHANDRASEKHAR-BANDARU@users.noreply.github.com>
2018-09-07 19:07:02 +0530

    Update 11.java

--- 11.java
+++ 11.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
-public class PowerOfNumber {
-   public static void main(String args[]){
+public class PowerOfNumber 
+{
+   public static void main(String args[])
+   {
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter the base number ::");
       int base = sc.nextInt();
@@ -8,7 +10,8 @@ public class PowerOfNumber {
       System.out.println("Enter the exponent number ::");
       int exp = sc.nextInt();
 
-      for (int i=1; i<exp; i++){
+      for (int i=1; i<exp; i++)
+      {
          temp = temp*temp;
       }
       System.out.println("Result of "+base+" power "+exp+" is "+temp);
